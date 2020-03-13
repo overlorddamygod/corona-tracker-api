f616059ef59bf3e3c33bd6a5340ad3318c31d5e7
overlorddamygod <overlord.damygod@gmail.com>
2020-03-13 23:15:54 +0545

    fixed

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const morgan = require('morgan');
+// const morgan = require('morgan');
 const cors = require('cors');
 
 const coronaAPI = require('./api/index');
@@ -10,7 +10,7 @@ const PORT = process.env.PORT || 5000;
 // Middlewares
 app.use(cors());
 app.use(express.json());
-app.use(morgan('tiny'));
+// app.use(morgan('tiny'));
 
 // Route Middlewares
 app.use('/api/corona/', coronaAPI);

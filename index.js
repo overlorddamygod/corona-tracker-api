578bd775b8a5be2dcce86d9fbbb60666c9441bc2
overlorddamygod <overlord.damygod@gmail.com>
2020-03-19 11:48:30 +0545

    FIXED

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-// const morgan = require('morgan');
+const dotenv = require('dotenv');
 const cors = require('cors');
 
 const coronaAPI = require('./api/index');
@@ -7,7 +7,9 @@ const coronaAPI = require('./api/index');
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+dotenv.config('./env')
 // Middlewares
+
 app.use(cors());
 app.use(express.json());
 // app.use(morgan('tiny'));

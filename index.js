078aa193c34279a48be47895630c461bc447e33c
overlorddamygod <overlord.damygod@gmail.com>
2020-09-09 11:33:33 +0545

    FOR DEPLOY

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const cors = require('cors');
 const coronaAPI = require('./api/index');
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 31030;
 
 dotenv.config('./env');
 
